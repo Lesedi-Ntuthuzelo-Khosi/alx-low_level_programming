c059f1629bb090087182e5733c23e089e8545d58
Lesedi-Ntuthuzelo-Khosi <khosilesedintuthuzelo@gmail.com>
2023-11-06 14:52:54 -0800

    2-main.c corrections

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -4,10 +4,12 @@
 /**
  * main -  prints the name of the file it was compiled from,
  *		followed by a new line.
+ *
+ * Return: 0 always(success)
  */
 int main(void)
 {
-	char *s = __BASE_FILE__;
+	char *s = __FILE__;
 
 	printf("%s\n", s);
 	return (0);
